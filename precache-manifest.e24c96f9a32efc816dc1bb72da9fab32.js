a6bb8dbace9bef5a44104cdc967cf1c7ffce5e9b
Zidan <midoz393@gmail.com>
2019-12-17 15:42:32 -0500

    Updates

---
+++ precache-manifest.e24c96f9a32efc816dc1bb72da9fab32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcc058e83fcd4dc9cbd32076b1713cb",
+    "url": "/grad-portfolios/index.html"
+  },
+  {
+    "revision": "3d1d2197b2840188789c",
+    "url": "/grad-portfolios/static/css/main.05569fa1.chunk.css"
+  },
+  {
+    "revision": "f998ec378cb8d4a97ec4",
+    "url": "/grad-portfolios/static/js/2.baff6445.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/grad-portfolios/static/js/2.baff6445.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d1d2197b2840188789c",
+    "url": "/grad-portfolios/static/js/main.c36d2597.chunk.js"
+  },
+  {
+    "revision": "1bdb5f8b5ca9da480efc",
+    "url": "/grad-portfolios/static/js/runtime-main.c33ea4c8.js"
+  },
+  {
+    "revision": "6d91d5298797bb10a8b1458e1fa7b808",
+    "url": "/grad-portfolios/static/media/jay.6d91d529.jpg"
+  }
+]);
